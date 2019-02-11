823f5f8a12dafcc7930893ae9120c908b423a63a
Сергей Мосин <mosinsv@yandex.ru>
2019-02-11 21:43:51 +0400

    Правки схемы

--- schema.sql
+++ schema.sql
@@ -5,25 +5,25 @@ CREATE DATABASE doingsdone
 USE doingsdone;
 
 CREATE TABLE categories (
-	id INT AUTO_INCREMENT PRIMARY_KEY,
+	id INT AUTO_INCREMENT PRIMARY KEY,
  	name CHAR(128),
  	user_id INT
 );
 
 CREATE TABLE tasks (
- 	id INT AUTO_INCREMENT PRIMARY_KEY,
+ 	id INT AUTO_INCREMENT PRIMARY KEY,
  	create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
  	decided_date TIMESTAMP,
  	result TINYINT DEFAULT 0,
  	name CHAR(128),
- 	file CHAR(256),
+ 	file CHAR(128),
  	deadline_date TIMESTAMP,
- 	category_id CHAR,
+ 	category_id INT,
  	user_id INT
 );
 
 CREATE TABLE users (
- 	id INT AUTO_INCREMENT PRIMARY_KEY,
+ 	id INT AUTO_INCREMENT PRIMARY KEY,
  	reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
  	email CHAR(128) NOT NULL UNIQUE,
  	username CHAR(128) NOT NULL UNIQUE,
